b3d01d81233a768e2eef1da07d2dc5f982c67c0c
mxnojkumar <manojkv5696@gmail.com>
2024-09-29 20:33:52 +0530

    Bootstrap for main page

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,19 +1,21 @@
 from typing import Any
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, MEAL_TYPE
 
 class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
     
-    def get_context_data(self):
-        context = {
-            "meals": 'Pizza',
-            "ingredients": ['Flour', 'Cheese']
-        }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
         return context
     
 class MenuItemDetail(generic.DetailView):
     model = Item
     template_name = "menu_item_detail.html"
+    
+class About(generic.TemplateView):
+    model = Item
+    template_name = "about.html"
